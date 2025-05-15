b13b233ea81242ca9b82922d4448119d5a8b78ba
viktoria3715 <147585653+viktoria3715@users.noreply.github.com>
2025-05-15 12:57:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: '.', // Корень проекта
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html')
+    }
+  }
 })
